572458ca781ca96ac2adac8c46c117b3263ca5a6
yonzalove123 <tarozee044@gmail.com>
2024-04-18 01:07:55 +0700

    Bug fix#2 - adding student id validation (id must be composed of numbers between 0 and 9)

--- validate.js
+++ validate.js
@@ -1,6 +1,6 @@
 function checkSID() {
   let sid = (document.getElementById("sid").value).trim();
-  if (sid.length == 10) {
+  if (sid.length == 10 && !isNaN(sid)) {
     return true;
   } else {
     return false;
